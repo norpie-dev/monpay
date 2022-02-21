9bf9e327d946399006bceb23ca98fafdce159d54
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-02-22 00:02:02 +0100

    rename __init__ to __main__

--- monpay/__init__.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("main func")
-
-if __name__ == "__main__":
-    main()
---
+++ monpay/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("MonPay is meant to be used as an imported module, not as a standalone program.")
+
+
+if __name__ == "__main__":
+    main()
